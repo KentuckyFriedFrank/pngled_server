14b58ee3ed2a450293bbb6973d51b2c9ef12f38e
MuffinsaursRex <jackyfryers@gmail.com>
2016-06-12 16:50:15 -0700

    Updated client.ino with FastLED / Pointer

--- client/client.ino
+++ client/client.ino
@@ -1,64 +1,40 @@
-#include <Adafruit_NeoPixel.h>
+#include "FastLED.h"
 
-#define LED_COUNT 150
-#define LED_DATA_PIN 3
-#define LED_DEFAULT_BRIGHTNESS 25
+// How many leds in your strip?
+#define NUM_LEDS 150
+#define BRIGHTNESS  255
 
-#define SERIAL_START_COMMAND 0x60
-#define SERIAL_BEGIN_ANIMATION_FRAME_COMMAND 0x0
+// For led chips like Neopixels, which have a data line, ground, and power, you just
+// need to define DATA_PIN.  For led chipsets that are SPI based (four wires - data, clock,
+// ground, and power), like the LPD8806 define both DATA_PIN and CLOCK_PIN
+#define DATA_PIN 2
 
-Adafruit_NeoPixel _pixels = Adafruit_NeoPixel(LED_COUNT, LED_DATA_PIN, NEO_GRB + NEO_KHZ800);
+// Define the array of leds
+CRGB leds[NUM_LEDS];
 
-void setup() {
-  Serial.begin(9600);
+void setup() { 
+      FastLED.addLeds<WS2812B, DATA_PIN, RGB>(leds, NUM_LEDS);
+  	  Serial.begin(115200);
+      FastLED.setBrightness(  BRIGHTNESS );
+      leds[0] = CRGB::Red;
+      FastLED.show();
+      delay(500);
+      // Now turn the LED off, then pause
+      leds[0] = CRGB::Black;
+      FastLED.show();
+      delay(500);
+}
+
+void loop() { 
   
-  _pixels.begin();
-  _pixels.setBrightness(LED_DEFAULT_BRIGHTNESS);
-
-  uint32_t red = _pixels.Color(255, 0, 0);
-  uint32_t green = _pixels.Color(0, 255, 0);
-  uint32_t blue = _pixels.Color(0, 0, 255);
-
-  _pixels.setPixelColor(0, red);
-  _pixels.setPixelColor(1, green);
-  _pixels.setPixelColor(2, blue);
-  _pixels.show();
-}
-
-uint8_t i;
-
-void loop() {
-  while (Serial.available() <= 2) {
-    // Wait for data.
+  int startChar = Serial.read();
+  if (startChar == '*') {
+     //debug: read one led color ( 3 bytes)
+     //Serial.readBytes( (char*)(&leds[5]), 3); // read three bytes: r, g, and b.
+     Serial.readBytes( (char*)leds, NUM_LEDS * 3);
+     FastLED.show();
   }
-
-  i = Serial.read();
-
-  // 0x60 0x00 <numPixels * 3 bytes>
-
-  if (i != SERIAL_START_COMMAND) {
-    return;
+  else if (startChar >= 0) {
+    // discard unknown characters
   }
-
-  i = Serial.read();
-
-  if (i == SERIAL_BEGIN_ANIMATION_FRAME_COMMAND) {
-    readFrame();
-  }
-}
-
-uint8_t r, g, b;
-
-void readFrame() {
-  for (uint8_t x = 0; x < LED_COUNT; ++x) {
-    while (Serial.available() < 3) {
-      // Wait for more pixel data.
-    }
-
-    r = Serial.read();
-    g = Serial.read();
-    b = Serial.read();
-    _pixels.setPixelColor(x, r, g, b);
-  }
-  _pixels.show();
 }
