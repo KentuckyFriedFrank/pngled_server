ba8ed5786f420765220bcd5e310a52bcaf9f4548
MuffinsaursRex <jackyfryers@gmail.com>
2016-06-12 15:27:08 -0700

    First commit

--- server.py
+++ server.py
@@ -1,47 +1,54 @@
 #bin/python
+import argparse
+parser = argparse.ArgumentParser(description='Process some integers.')
+parser.add_argument('--fn', dest='filename',action="store", type=str, help="filename")
+args = parser.parse_args()
+print args.filename
+
 
 import numpy
 import serial
+from serial.tools import list_ports
 import time
 from PIL import Image
 from twisted.internet import reactor
 
 class Player(object):
-	def __init__(self, imagePath, frameOutClosure):
-		self.imagePath = imagePath
-		self.out = frameOutClosure
-		self.fps = 30
-		self.reloadImage()
+        def __init__(self, imagePath, frameOutClosure):
+                self.imagePath = imagePath
+                self.out = frameOutClosure
+                self.fps = 30
+                self.reloadImage()
 
-	def reloadImage(self):
-		print "reloading %s" % self.imagePath
-		i = Image.open(self.imagePath).convert('RGB')
-		self.image = numpy.asarray(i)
-		print self.image.shape
+        def reloadImage(self):
+                print "reloading %s" % self.imagePath
+                i = Image.open(self.imagePath).convert('RGB')
+                self.image = numpy.asarray(i)
+                print self.image.shape
 
-	def step(self):
-		now = time.time()
-		frameNum = int ((now * self.fps) % self.image.shape[1])
-		frame = self.image[::-1,frameNum,:]
-		self.out(frame)
+        def step(self):
+                now = time.time()
+                frameNum = int ((now * self.fps) % self.image.shape[1])
+                frame = self.image[::-1,frameNum,:]
+                self.out(frame)
 
 if __name__ == "__main__":
-	filename = "white-150.png"
-	port = serial.Serial('/dev/cu.usbserial-AD01V6V1', baudrate=9600)
+        filename = args.filename
+        ports = [port[0] for port in list_ports.comports()]
+        print "Using serial port %s; found %s" % (ports[-1], ports)
+        port = serial = serial.Serial(ports[-1],baudrate=115200, timeout=0, writeTimeout=1)
 
-	def frameOut(colors):
-		port.write('\x60\x00')
-		for color in colors:
-			#port.write(chr(int(color[2])) + chr(int(color[0])) + chr(int(color[1])))
-			print "%d %d %d" % (int(color[0]), int(color[1]), int(color[2]))
-			port.write(chr(int(color[0])) + chr(int(color[1])) + chr(int(color[2])))
-		port.flush()
+        def frameOut(colors):
+                foo = colors.copy(order='C')
+                port.write(str.encode('*'))
+                print('*')
+                print(foo)
+                port.write(foo)
+        player = Player(filename, frameOut)
 
-	player = Player(filename, frameOut)
+        def loop():
+                player.step()
+               reactor.callLater(0.01, loop)
 
-	def loop():
-		player.step()
-		reactor.callLater(0.01, loop)
-
-	loop()
-	reactor.run()
+        loop()
+        reactor.run()
